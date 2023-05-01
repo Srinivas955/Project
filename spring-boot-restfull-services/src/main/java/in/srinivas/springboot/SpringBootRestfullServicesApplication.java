0cc272d66bf62bf77b5a98e0f769c8370adaff59
Srinivas Yadav <kookatlasrinivasyadav@gmail.com>
2023-05-01 17:13:03 +0530

    created springbootrestfullwebservices project

---
+++ spring-boot-restfull-services/src/main/java/in/srinivas/springboot/SpringBootRestfullServicesApplication.java
@@ -0,0 +1,13 @@
+package in.srinivas.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfullServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfullServicesApplication.class, args);
+	}
+
+}
